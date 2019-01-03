496e3d020e0f7e902c3ce77892103fdc684a6fdb
orange <1067357662@qq.com>
2019-01-03 19:42:51 +0800

    更新

--- admin-web-page/config/dev.env.js
+++ admin-web-page/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:80"',
+  BASE_API: '"http://localhost:80/verify"',
 })
