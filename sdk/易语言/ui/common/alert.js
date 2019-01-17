1ef78cc3943b830b1e1c17e540678a66884757b5
orange <1067357662@qq.com>
2019-01-17 22:36:19 +0800

    优化代码 增加运行日志，增加开放接口后台管理，增强软件日志，优化易语言demo,优化后台页面增加loading

--- sdk/易语言/ui/common/alert.js
+++ sdk/易语言/ui/common/alert.js
@@ -1,9 +1,21 @@
 
+var index;
+
+loadingStart();
 function loadingStart() {
-    $("body").mLoading("show");
+    //$("body").mLoading("show");
+    layui.use("layer",function(){
+        index = layer.load(1, {
+            shade: [0.1,'#919191']
+        });
+    });
+
 }
 function loadingClose() {
-    $("body").mLoading("hide");
+    //$("body").mLoading("hide");
+    layui.use("layer",function(){
+        layer.close(index);
+    });
 }
 function layerAlert(content) {
     layui.use("layer",function(){
@@ -11,3 +23,5 @@ function layerAlert(content) {
         layer.msg(content);
     });
 }
+
+
